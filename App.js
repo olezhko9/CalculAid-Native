be274dfbf67859b0876bc9981e963e3cb2cf2329
Oleg Naumov <oleg.naumov.98@mail.ru>
2020-01-12 04:36:32 +0300

    change ui framework to material paper

--- App.js
+++ App.js
@@ -1,97 +1,38 @@
-import React, { Component } from "react";
-import { Container, Header, Content, List, ListItem, Text, Tabs, Tab } from "native-base";
-import axios from 'axios';
+import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+import Tab1 from './components/Tab1';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: "key1",
-      products: [],
-      speech: "Я съел 2 куска ржаного хлеба еще я съел десять чайных ложек варенного риса а еще выпил двести миллилитров апельсинного сока и еще выпил стакан козьего молока а еще я съел одно яблоко"
-    };
-  }
+// const calculatorRoute = () => <Text>calculator</Text>;
 
-  componentDidMount(): void {
-    this.getData()
-  }
+const AlbumsRoute = () => <Text>Albums</Text>;
 
-  async getData() {
-    try {
-      const response = (await axios.post('http://194.87.101.20:3000/api/products', {
-        speech: this.state.speech
-      })).data
-      this.setState({
-        products: response.data
-      })
-    } catch (e) {
-      console.log(e);
-    }
-  }
+const RecentsRoute = () => <Text>Recents</Text>;
+
+export default class MyComponent extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'calculator', title: 'Калькулятор', icon: 'calculator' },
+      { key: 'albums', title: 'Дневник', icon: 'book' },
+      { key: 'recents', title: 'Статистика', icon: 'chart-bar' },
+    ]
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    calculator: Tab1,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
 
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <List>
-            {
-              this.state.products.map((product, index) =>
-                <ListItem key={index}>
-                  <Text>{ product.products[0].name }</Text>
-                </ListItem>
-              )
-            }
-          </List>
-        </Content>
-        <Tabs tabBarPosition={'bottom'}>
-          <Tab heading="Tab1">
-          </Tab>
-          <Tab heading="Tab2">
-          </Tab>
-          <Tab heading="Tab3">
-          </Tab>
-        </Tabs>
-      </Container>
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.dark,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
---
+++ components/Tab1.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import axios from 'axios';
+
+import {List, Divider, IconButton, Text, Chip, Button} from 'react-native-paper';
+import { StyleSheet, FlatList, View } from 'react-native';
+
+
+export default class Tab1 extends React.Component {
+  state = {
+    products: [],
+    speech: "Я съел 2 куска ржаного хлеба еще я съел десять чайных ложек варенного риса а еще выпил двести миллилитров апельсинного сока и еще выпил стакан козьего молока а еще я съел одно яблоко"
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  async getData() {
+    try {
+      const response = (await axios.post('http://194.87.101.20:3000/api/products', {
+        speech: this.state.speech
+      })).data
+      this.setState({
+        products: response.data
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    if (this.state.products.length) {
+      return (
+        <FlatList
+          ItemSeparatorComponent={Divider}
+          renderItem={({item}) => (
+            <List.Item
+              title={item.products[0].name.charAt(0).toUpperCase() + item.products[0].name.slice(1)}
+              titleNumberOfLines={2}
+              titleStyle={{fontWeight: 'bold', fontSize: 18}}
+              description={() => (
+                <View>
+                  <View style={[styles.row, { paddingTop: 8 }]}>
+                    <Text>{`Количество: ${item.amount} ${item.products[0].measures[0].name}`}</Text>
+                  </View>
+                  <View style={[styles.row, { paddingTop: 8 }]}>
+                    <Chip style={[styles.chip]}>
+                      {`Б: ${item.products[0].pfc.p}`}
+                    </Chip>
+                    <Chip style={[styles.chip]}>
+                      {`Ж: ${item.products[0].pfc.f}`}
+                    </Chip>
+                    <Chip style={[styles.chip]}>
+                      {`У: ${item.products[0].pfc.c}`}
+                    </Chip>
+                  </View>
+                </View>
+              )}
+              right={props => (
+                <View style={[styles.column, {justifyContent: 'center'}]}>
+                  <Button onPress={() => console.log('Pressed')}>
+                    Сменить
+                  </Button>
+                </View>
+              )}>
+            </List.Item>
+          )}
+          keyExtractor={(item) => item.products[0].id.toString()}
+          data={this.state.products}
+        />
+      )
+    }
+
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  chip: {
+    marginRight: 16
+  }
+});
